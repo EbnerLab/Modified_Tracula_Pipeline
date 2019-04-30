0f8b5500eb2761b1207eaa4d92aab6be09dd8822
Désirée Lussier <9257980+dllussier@users.noreply.github.com>
2019-04-30 11:52:34 -0400

    Update and rename DTI_pipeline_v3_4.sh to pipeline_step_04.sh

--- pipeline_step_04.sh
+++ pipeline_step_04.sh
@@ -3,8 +3,6 @@
 #loads appropriate modules
 module load freesurfer/6.0.0
 module load fsl/5.0.10
-module load mricron
 
 #starts trac scripts
-sbatch tracall_priors_01.sh
-sbatch tracall_priors_02.sh
+sbatch tracall_priors.sh
